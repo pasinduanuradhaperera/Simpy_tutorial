206e75217994261bb01c563871471daf4912cbe7
Pasindu Perera <pasinduanuradhaperera@gmail.com>
2024-09-06 16:32:27 +0530

    change direcotories

--- examples/Theoretical Examples/colectingData.py
+++ examples/Theoretical Examples/colectingData.py
@@ -5,12 +5,14 @@ def car(env, name, charging_station, wait_times):
     print(f'{name} arriving at {env.now}')
     
     with charging_station.request() as request:
+        print(type(wait_times))
         yield request
         wait_time = env.now - arrival_time
         wait_times.append(wait_time)  # Collect wait time
         print(f'{name} started charging after waiting {wait_time} units')
         yield env.timeout(2)
         print(f'{name} finished at {env.now}')
+        print(wait_times)
 
 env = simpy.Environment()
 charging_station = simpy.Resource(env, capacity=2)
@@ -18,6 +20,7 @@ wait_times = []
 
 for i in range(5):
     env.process(car(env, f'Car {i}', charging_station, wait_times))
+    print(wait_times)
 
 env.run()
 
